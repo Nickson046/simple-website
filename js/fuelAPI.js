0eb3cd2d16aaaf2deef22e43ef60fc7318510aa9
Nick <sentongosmart@gmail.com>
2026-03-14 13:31:51 +0200

    Improved the admin status to update prices

--- js/fuelAPI.js
+++ js/fuelAPI.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", () => {
+/*document.addEventListener("DOMContentLoaded", () => {
 
 const petrolElement = document.getElementById("petrolPrice");
 const dieselElement = document.getElementById("dieselPrice");
@@ -11,4 +11,25 @@ let diesel = localStorage.getItem("diesel") || 1.38;
 petrolElement.textContent = petrol;
 dieselElement.textContent = diesel;
 
-});
+});*/
+
+/*new code */
+import { db } from "./firebase.js";
+import { doc, getDoc } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-firestore.js";
+
+async function loadPrices() {
+
+  const docRef = doc(db, "fuel_prices", "prices");
+  const docSnap = await getDoc(docRef);
+
+  if (docSnap.exists()) {
+
+    const data = docSnap.data();
+
+    document.getElementById("petrolPrice").innerText = "$" + data.petrol;
+    document.getElementById("dieselPrice").innerText = "$" + data.diesel;
+
+  }
+}
+
+loadPrices();
