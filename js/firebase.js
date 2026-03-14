51ee9da8dfc15df166b641167193b7208e76cd39
Nick <sentongosmart@gmail.com>
2026-03-14 03:20:52 +0200

    added a logo and improved still working on the backend for the admin

--- js/firebase.js
+++ js/firebase.js
@@ -1,21 +1,42 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-app.js";
 
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
-
-import { getAuth } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+import {
+getFirestore,
+doc,
+getDoc
+} from "https://www.gstatic.com/firebasejs/12.10.0/firebase-firestore.js";
 
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "redfuel-a2152.firebaseapp.com",
-  projectId: "redfuel-a2152",
-  storageBucket: "redfuel-a2152.firebasestorage.app",
-  messagingSenderId: "782154176195",
-  appId: "1:782154176195:web:46af20e4252e415ba77476"
+apiKey: "AIzaSyB-Q-MFZDCgHnaVgMRrgxEL9YPrIqZwUrU",
+authDomain: "redfuel-a2152.firebaseapp.com",
+projectId: "redfuel-a2152",
+storageBucket: "redfuel-a2152.firebasestorage.app",
+messagingSenderId: "782154176195",
+appId: "1:782154176195:web:46af20e4252e415ba77476"
 };
 
 
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
-export const db = getFirestore(app);
-export const auth = getAuth(app);
+
+async function loadPrices(){
+
+const ref = doc(db,"fuelPrices","current");
+
+const snap = await getDoc(ref);
+
+if(snap.exists()){
+
+document.getElementById("petrolPrice").innerText =
+"$" + snap.data().petrol;
+
+document.getElementById("dieselPrice").innerText =
+"$" + snap.data().diesel;
+
+}
+
+}
+
+loadPrices();
